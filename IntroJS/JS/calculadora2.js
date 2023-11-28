4316a93648cf04bee710d2998131b23fe743d50d
LARYSSA MACHADO HUGEM <laryssa.mhugem@senacsp.edu.br>
2023-11-27 21:57:39 -0300

    JS aula seletor 27 11

--- IntroJS/JS/calculadora2.js
+++ IntroJS/JS/calculadora2.js
@@ -1,6 +1,30 @@
-function calcular() {
-    const primeiroNumero = document.formCalculadora.num1.value;
-    const segundoNumero = document.formCalculadora.num2.value;
+// pegando o valor da caixa de texto via querySelector
+// console.log(document.querySelector('#numero1').value);
+
+// pegando o valor da caixa de texto via getElementById
+// console.log(document.getElementById('numero1').value);
+
+// pegando o valor da caixa de texto via getElementsByTagName
+// Importante getElementsByTagName retorna um array de tags
+// console.log(document.getElementsByTagName('input')[0].value);
+
+// pegando o valor da caixa de texto via getElementsByClassName
+// Importante getElementsByClassName retorna um array de elementos
+// Com a referida classe
+// console.log(document.getElementsByClassName('form-input')[0].value);
+
+// alterando valores de uma tag utilizando o innerHTML
+// console.log(document.getElementsByTagName('label')[0].innerHTML='Meu número');
+
+// capturrando valores de uma tag utilizando o innerHTML
+// console.log(document.getElementsByTagName('label')[0].innerHTML);
+
+
+
+
+function Calcular() {
+    const primeiroNumero = parseFloat (document.formCalculadora.num1.value);
+    const segundoNumero = parseFloat (document.formCalculadora.num2.value);
     const operacao = document.formCalculadora.oper.value;
     let resultado;
 
@@ -24,10 +48,21 @@ function calcular() {
 
 
     // janela 
-    alert('O resultado da operação é ', resultado);
+    // alert('O resultado da operação é ' + resultado);
 
     // console log
-    console.log('O resultado da operação é ', resultado);
+    // console.log('O resultado da operação é ', resultado);
     // documento
-    document.write('O resultado da operação é ', resultado);
+    // document.write('O resultado da operação é ', resultado);
+    const caixaresultado = document.getElementById('resultado');
+    caixaresultado.value=resultado;
+
+    const paragrafoResultado=document.getElementById('pResultado');
+    paragrafoResultado.innerHTML=resultado;
+
+    //Manipulando CSS via JS 
+    paragrafoResultado.style.color ='green';
+    paragrafoResultado.style.fontFamily = 'Arial';
+    paragrafoResultado.style.fontSize = 'larger';
+
 }
